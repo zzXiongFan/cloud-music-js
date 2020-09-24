43325cfe08d6aa76bd33bfd6b9cec4bea93fd6c9
zzxiongfan <zzxiongfan@gmail.com>
2020-09-24 15:00:10 +0800

    feat: 加入 iconfont

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+// 引入全局样式
+import { GlobalStyle } from './style';
+import { IconStyle }  from './assets/iconfont/iconfont';
 
 function App() {
   return (
     <div>
-      Home
+      <GlobalStyle />
+      <IconStyle />
+      <span className="iconfont">&#xe8ef;</span>
     </div>
   )
 }

ce6a946dc9ffeb2d04cbbd1f7ea22fa4094d17d8
zzxiongfan <zzxiongfan@gmail.com>
2020-10-12 17:28:52 +0800

    feat: 增加 slider 组件

---
+++ src/components/slider/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { IBanner } from '../../common';
+import { SliderContainer } from './style';
+import "swiper/dist/css/swiper.css";
+import Swiper from 'swiper';
+
+interface IProps {
+  bannerList: Array<IBanner>
+}
+
+function Slider({bannerList}: IProps) {
+  // 创建 Swiper 实例
+  const [ slideSwiper, setSlideSwiper ] = useState<Swiper | null>(null);
+  // 获取 bannerList 实例
+  // const { bannerList } = props;
+  useEffect(() => {
+    // 当 有数据时 渲染列表
+    if(!bannerList.length || slideSwiper) return;
+    const newSliderSwiper = new Swiper(".slider-container", {
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {el:'.swiper-pagination'},
+    });
+    setSlideSwiper(newSliderSwiper);
+  }, [bannerList.length, slideSwiper])
+  return (
+    <SliderContainer>
+      <div className="before"></div>
+      <div className="slider-container">
+        <div className="swiper-wrapper">
+          {
+            bannerList.map((item, index) => {
+              return (
+                <div className="swiper-slide" key={item.imageUrl + index}>
+                  <div className="slider-nav">
+                    <img src={item.imageUrl} width="100%" height="100%" alt="推荐" />
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="swiper-pagination"></div>
+      </div>
+    </SliderContainer>
+  )
+}
+
+export default React.memo(Slider);

e8ba3cdfee402dbb93ba970f54efe5ada2ecff16
zzxiongfan <zzxiongfan@gmail.com>
2020-10-15 22:03:24 +0800

    fix: 修复请求接口定义

--- src/api/request.ts
+++ src/api/request.ts
@@ -3,9 +3,9 @@ import { IBanner, IRecommend } from '../common';
 
 // 请求推荐列表: 进阶： 加入了类型参数
 export const getBannerRequest = () => {
-  return axiosInstance.get<IBanner>('/banner');
+  return axiosInstance.get<IBanner[]>('/banner');
 }
 
 export const getRecommendListRequest = () => {
-  return axiosInstance.get<IRecommend>('/personalized');
+  return axiosInstance.get<IRecommend[]>('/personalized');
 }

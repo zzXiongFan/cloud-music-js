d99b48f01b4f6d1cb5df156fdb47b88b8b298787
zzxiongfan <zzxiongfan@gmail.com>
2020-11-10 21:11:41 +0800

    feat($store/reducer): 合并 singers store

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,15 +1,18 @@
 import { combineReducers } from 'redux-immutable';
 import { Collection } from 'immutable';
 import { reducer as recommendReducer, IRecommendState } from '../application/Recommend/store/index';
+import { reducer as singgerReducer, ISingersState} from '../application/Singers/store/index';
 
 let rootReducer = combineReducers ({
   recommend: recommendReducer,
+  singers: singgerReducer,
 });
 
 export default rootReducer;
 
-type IState = IRecommendState;
+type IState = IRecommendState | ISingersState;
 
 export interface AppState extends Collection<string, IState> {
-  recommend: IRecommendState
+  recommend: IRecommendState,
+  singers: ISingersState,
 };

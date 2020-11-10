90e093969f7cae6c248d81b27e21ad688d2d7aba
zzxiongfan <zzxiongfan@gmail.com>
2020-11-10 21:05:18 +0800

    feat($api): 更新后端接口

---
+++ src/api/constants.ts
@@ -0,0 +1,18 @@
+export interface IArtists {
+  "img1v1Id": Number,
+  "topicPerson": Number,
+  "alias": Array<any>,
+  "picId": Number,
+  "musicSize": Number,
+  "albumSize": Number,
+  "briefDesc": string,
+  "followed": false,
+  "img1v1Url": string,
+  "trans": string,
+  "picUrl": string,
+  "name": string,
+  "id": Number,
+  "accountId": Number,
+  "picId_str": string,
+  "img1v1Id_str": string
+}
--- src/api/request.ts
+++ src/api/request.ts
@@ -1,5 +1,6 @@
 import { axiosInstance } from './config';
 import { IBanner, IRecommend } from '../application/Recommend/store/constants';
+import * as respType from './constants'
 
 // 请求推荐列表: 进阶： 加入了类型参数
 export const getBannerRequest = () => {
@@ -8,4 +9,12 @@ export const getBannerRequest = () => {
 
 export const getRecommendListRequest = () => {
   return axiosInstance.get<{result: IRecommend[]}>('/personalized');
+}
+
+export const getHotSingerListRequest = (count: Number) => {
+  return axiosInstance.get<{artists: respType.IArtists[]}>(`/top/artists?offset=${count}`);
+}
+
+export const getSingerListRequest= (category: String, alpha: String, count: Number) => {
+  return axiosInstance.get<{artists: respType.IArtists[]}>(`/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`);
 }
